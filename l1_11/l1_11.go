68e4ffbffbf2e1f65758b8c5f241d314e80b1aca
Danis <dandijar@yandex.ru>
2025-09-03 19:26:57 +0500

    l1.11 with map

--- l1_11/l1_11.go
+++ l1_11/l1_11.go
@@ -43,12 +43,59 @@ func intersection(a []int, b []int) []int {
 	return result
 }
 
+// intersectionWithMap делает то же самое что ли intersection,
+// но сохраняет значения как ключи в мапе
+// их уникальность легко считается
+//
+// все значения пишутся в resultMap
+//
+// ключи из A пишутся в keysInA
+//
+//	и resultMap (со значением false, т.к. сами по себе ещё ничего не значат - вдруг в B их не окажется)
+//
+// если в B нашлось значение из keysInA, то в resultMap ему задаётся значение true
+//
+// если в resultMap значение true, значит значение встретилось в обоих множествах,
+//
+//	иначе только в A
+//
+// использовать одну мапу опасно - неуникальные значения из B могут быть распознаны как встреченные в A
+//
+// как в l1.12 - насохранял в мапу и вернул типа умный
+func intersectionWithMap(a []int, b []int) []int {
+	resultMap := make(map[int]bool)
+
+	keysInA := make(map[int]struct{})
+
+	// записать ключи A
+	for _, v := range a {
+		resultMap[v] = false
+		keysInA[v] = struct{}{}
+	}
+	// записать ключи B
+	for _, v := range b {
+		if _, ok := keysInA[v]; ok {
+			resultMap[v] = true
+		}
+	}
+
+	resultSlice := make([]int, 0, len(resultMap))
+	for k, v := range resultMap {
+		if v {
+			resultSlice = append(resultSlice, k)
+		}
+	}
+	return resultSlice
+}
+
 func main() {
 	// можно взять любые числа
 	a := []int{1, 2, 3, 10}
 	b := []int{2, 3, 4, 5, 10}
 
 	fmt.Println(a, b, "пересечение", intersection(a, b))
+	fmt.Println(a, b, "пересечение быстрое", intersectionWithMap(a, b))
 
 	// [1 2 3 10] [2 3 4 5 10] пересечение [2 3 10]
+	// [1 2 3 10] [2 3 4 5 10] пересечение быстрое [2 3 10]
 }
